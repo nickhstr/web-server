2c70adddb50dfcba18262bda8f5baef517619df8
Nick Hester <nickhstr@gmail.com>
2016-05-14 12:18:02 -0700

    Initialize repo

---
+++ middleware.js
@@ -0,0 +1,13 @@
+let middleware = {
+	requireAuthentication: function(req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function(req, res, next) {
+		let time  = new Date().toString();
+		console.log('Request: ' + req.method + ' ' + req.originalUrl + ' at ' + time);
+		next();
+	}
+};
+
+module.exports = middleware;
